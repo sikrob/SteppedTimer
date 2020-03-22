a99e721fda28cd3b381baa1393a2a9a17bc02318
Robert Sikorski <sikorski.rob@gmail.com>
2020-03-22 00:22:36 -0400

    figuring out some level of ContentView planning

--- Stepped Timer/Views/ContentView/ContentView.swift
+++ Stepped Timer/Views/ContentView/ContentView.swift
@@ -8,26 +8,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View {  
   @State private var toolbarPlayImageName = "play.fill"
   @State private var toolbarStopImageName = "stop.fill"
 
-  private func toolbarPlayButtonCallback() {
+  private func toolbarPlayButtonAction() {
+    // determine timer state
+    // set update
+    // set new image name
     toolbarPlayImageName = "pause"
+  }
+  private func toolbarStopButtonAction() {
+    toolbarStopImageName = "stop"
   }
 
   var body: some View {
     let timerToolbarParams = TimerToolbarParams(
-      playCallback: toolbarPlayButtonCallback,
-      stopCallback: { },
+      playCallback: toolbarPlayButtonAction,
+      stopCallback: toolbarStopButtonAction,
       playImageSystemName: toolbarPlayImageName,
       stopImageSystemName: toolbarStopImageName)
 
     return VStack {
-      Text("Yo!")
-      Text("Yo!")
-      Text("Yo!")
-      Text("Yo!")
       TimerToolbar(params: timerToolbarParams)
     }
   }
---
+++ Stepped Timer/Views/ContentView/ContentViewLogic.swift
@@ -0,0 +1,7 @@
+//
+//  ContentViewLogic.swift
+//  Stepped Timer
+//
+//  Created by Robert Sikorski on 3/22/20.
+//  Copyright © 2020 Robert Sikorski. All rights reserved.
+//
