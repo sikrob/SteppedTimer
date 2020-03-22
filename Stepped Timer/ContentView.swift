e2837dd752ef717847133fde423485a561916a9f
Robert Sikorski <sikorski.rob@gmail.com>
2020-03-21 21:18:09 -0400

    Initial Commit

---
+++ Stepped Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stepped Timer
+//
+//  Created by Robert Sikorski on 3/21/20.
+//  Copyright © 2020 Robert Sikorski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
