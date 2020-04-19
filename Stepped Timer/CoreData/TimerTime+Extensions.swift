ebab18b295a04d7e13b63c737d6dbfabdac5fcd0
Robert Sikorski <sikorski.rob@gmail.com>
2020-04-19 00:51:47 -0400

    initial commit README.md

---
+++ Stepped Timer/CoreData/TimerTime+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  TimerTime+Extensions.swift
+//  Stepped Timer
+//
+//  Created by Robert Sikorski on 4/19/20.
+//  Copyright © 2020 Robert Sikorski. All rights reserved.
+//
+
+import Foundation
